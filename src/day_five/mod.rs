973fc07b539ed680e67af27f655356457fac68e5
Daniel Runge Petersen <rungedaniel@hotmail.com>
2024-12-09 14:04:37 +0100

    feature: solve day 5 puzzle 2

--- src/day_five/mod.rs
+++ src/day_five/mod.rs
@@ -1,3 +1,4 @@
+use std::cmp::Ordering;
 use std::collections::hash_map::Entry;
 use std::collections::{HashMap, HashSet};
 use std::fs::File;
@@ -11,8 +12,7 @@ fn parse(filename: &str) -> Result<(HashMap<i32, HashSet<i32>>, Vec<Vec<i32>>), 
     let mut updates = Vec::<Vec<i32>>::new();
     for line in reader.lines() {
         let line = line?;
-        if let [first, second, ..] =
-            &line.split("|").map(String::from).collect::<Vec<String>>()[..]
+        if let [first, second, ..] = &line.split("|").map(String::from).collect::<Vec<String>>()[..]
         {
             let first = first.parse::<i32>().unwrap();
             let second = second.parse::<i32>().unwrap();
@@ -46,13 +46,40 @@ pub fn day_5_puzzle_1(filename: &str) {
         .filter(|page| {
             page.is_sorted_by(|first, second| !rules.get(first).unwrap().contains(second))
         })
-        .map(|page| {page[page.len() /2]}).sum::<i32>();
+        .map(|page| page[page.len() / 2])
+        .sum::<i32>();
 
     println!("Day 5 Puzzle 1 solution: {result}");
 }
 
 pub fn day_5_puzzle_2(filename: &str) {
-    let result = 0;
+    let (rules, updates) = parse(filename).unwrap();
+    let result = updates
+        .into_iter()
+        .filter(|page| {
+            !page.is_sorted_by(|first, second| {
+                // println!("{:?} -> {:?}", first, second);
+                !rules.get(first).unwrap().contains(second)
+            })
+        })
+        .map(|mut incorrect_page| {
+            incorrect_page.sort_by(|first, second| {
+                if let Some(before) = rules.get(first) {
+                    if before.contains(second) {
+                        return Ordering::Less;
+                    }
+                }
+                if let Some(after) = rules.get(second) {
+                    if after.contains(first) {
+                        return Ordering::Greater;
+                    }
+                }
+                Ordering::Equal
+            });
+            incorrect_page
+        })
+        .map(|page| page[page.len() / 2])
+        .sum::<i32>();
 
-    println!("Day 5 Puzzle 1 solution: {filename}");
+    println!("Day 5 Puzzle 2 solution: {result}");
 }
